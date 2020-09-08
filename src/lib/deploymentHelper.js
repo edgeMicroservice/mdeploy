9575109acab68faca7d84046b608ab59f7508293
Jeremy Hsu <jeremy.hsu@mimik.com>
2020-09-08 11:55:15 -0700

    EPOC-317 added supports for dynamic http port

--- src/lib/deploymentHelper.js
+++ src/lib/deploymentHelper.js
@@ -1,15 +1,17 @@
 const { rpAuth, SERVICE_CONSTANTS } = require('./auth-helper');
 const { extractFromServiceType } = require('../util/serviceNameHelper');
 
-const JSONRPC_URL = 'http://127.0.0.1:8083/jsonrpc/v1';
 const JSONRPC_VERSION = '2.0';
 const JSONRPC_METHOD = 'getEdgeHmacCode';
-
-const MCM_URL = 'http://127.0.0.1:8083/mcm/v1';
 
 const HMAC_EXPIRES_IN = 60; // in seconds
 
 const makeDeploymentHelper = (context) => {
+  const { httpPort } = context.info;
+
+  const JSONRPC_URL = `http://127.0.0.1:${httpPort}/jsonrpc/v1`;
+  const MCM_URL = `http://127.0.0.1:${httpPort}/mcm/v1`;
+
   const generateHmac = (nodeId, imageId, accessToken) => {
     const options = {
       url: JSONRPC_URL,
