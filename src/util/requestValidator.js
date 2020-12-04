7f7c09718ca7085408abebd8cde7dd9060d5d1b9
Zhi Ye <zhi.ye@mimik.com>
2020-12-04 13:05:28 -0800

    EPOC-374 return reject

--- src/util/requestValidator.js
+++ src/util/requestValidator.js
@@ -2,14 +2,19 @@ const Promise = require('bluebird');
 
 const checkNewImageParams = (newImage) => new Promise((resolve, reject) => {
   const { imageHostNodeId, imageUrl, imageId } = newImage;
+  let errorMessage;
+
   if (!imageUrl && !imageHostNodeId) {
-    reject(new Error('Must provide either imageUrl or imageHostNodeId.'));
-  } else if (imageUrl && imageHostNodeId) {
-    reject(new Error('Provide either imageUrl or imageHostNodeId but not both.'));
-  } else if (imageHostNodeId && !imageId) {
-    reject(new Error('Provide imageId is required with imageHostNodeId.'));
+    errorMessage = new Error('Must provide either imageUrl or imageHostNodeId.');
+  } if (imageUrl && imageHostNodeId) {
+    errorMessage = new Error('Provide either imageUrl or imageHostNodeId but not both.');
+  } if (imageHostNodeId && !imageId) {
+    errorMessage = new Error('Provide imageId is required with imageHostNodeId.');
   }
-  return resolve();
+
+  if (!errorMessage) return resolve();
+
+  return reject(errorMessage);
 });
 
 module.exports = {
